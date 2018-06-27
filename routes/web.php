34df5c48a004150a4bbbd50e479a30269dadeacf
5adiyah <sadiali@pdx.edu>
2018-06-27 14:16:02 -0700

    Add create events route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 Route::get('/', 'EventsController@home');
 Route::get('/events', 'EventsController@index');
+Route::get('/events/create', 'EventsController@create');
 Route::get('events/{event}', 'EventsController@show');
 Route::get('/past-events', 'EventsController@pastEvents');
 Route::get('/current-events', 'EventsController@currentEvents');
