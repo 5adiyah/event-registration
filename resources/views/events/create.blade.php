7f45e32bdd5cddc7b9a306bd2f17312f80531f50
5adiyah <sadiali@pdx.edu>
2018-06-27 15:13:29 -0700

    Add form

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -5,32 +5,24 @@
 
     <h3>Create An Event</h3>
 
-    <div class="eventForm">
-      <form>
-        <div class="row">
-          <div class="six columns">
-            <label for="exampleEmailInput">Your email</label>
-            <input class="u-full-width" type="email" placeholder="test@mailbox.com" id="exampleEmailInput">
-          </div>
-          <div class="six columns">
-            <label for="exampleRecipientInput">Reason for contacting</label>
-            <select class="u-full-width" id="exampleRecipientInput">
-              <option value="Option 1">Questions</option>
-              <option value="Option 2">Admiration</option>
-              <option value="Option 3">Can I get your number?</option>
-            </select>
-          </div>
-        </div>
-        <label for="exampleMessage">Message</label>
-        <textarea class="u-full-width" placeholder="Hi Dave …" id="exampleMessage"></textarea>
-        <label class="example-send-yourself-copy">
-          <input type="checkbox">
-          <span class="label-body">Send a copy to yourself</span>
-        </label>
-        <input class="button-primary" type="submit" value="Submit">
-      </form>
-    </div>
-    
+    <form>
+      <label for="eventName">Event Name</label>
+      <input class="u-full-width" type="text" id="eventName" name="eventName">
+
+      <label for="eventStatus">Event Type</label>
+      <input class="u-full-width" type="text" id="eventStatus" name="eventStatus">
+
+      {{-- <label for="eventStatus">Event Type</label>
+      <select class="u-full-width" id="exampleRecipientInput">
+        <option value="past">Past Event</option>
+        <option value="current">Current Event</option>
+        <option value="future">Future Event</option>
+      </select> --}}
+
+      <input class="button-primary" type="submit" value="Submit">
+
+    </form>
+
   </div>
 
 @endsection
