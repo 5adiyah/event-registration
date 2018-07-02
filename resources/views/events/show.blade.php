42012b27d90e526307da2657cb551beed736f55d
5adiyah <sadiali@pdx.edu>
2018-07-02 14:50:52 -0700

    Change styling on individual event page

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
   <div class="eventContainer">
-    <h3>{{ $event->title }}</h3> <hr>
+    <h3>{{ $event->title }}</h3>
     <div class="eventFlyer">
       <img src="/storage/{{ $event->imageUrl }}">
     </div>
@@ -12,19 +12,13 @@
       <p><strong>End Date:</strong> {{ $event->endDate }}</p>
       <p><strong>Location:</strong> {{ $event->location }}</p>
       <p>{!! $event->description !!}</p>
+
+      <div class="tickets links">
+        @foreach ($event->tickets as $ticket)
+            <a href="/events/{{ $event->id }}/tickets/{{ $ticket->id }}"><h5 class="ticketLink">{{ $ticket->type }} Ticket:&nbsp; ${{ $ticket->price }}</h5></a>
+        @endforeach
+      </div>
     </div>
-  </div>
-
-  <div class="container">
-
-    <div class="tickets links">
-      @foreach ($event->tickets as $ticket)
-          <a href="/events/{{ $event->id }}/tickets/{{ $ticket->id }}"><h5 class="ticketLink">{{ $ticket->type }} Ticket:&nbsp; ${{ $ticket->price }}</h5></a>
-      @endforeach
-    </div>
-
-    <hr>
-
   </div>
 
 @endsection
